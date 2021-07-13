30fefea538b5fedad3194f3795f36c6bf2c55302
dadawu-hot <xinyiwu1202@163.com>
2021-07-13 16:53:14 +0800

    first commit

---
+++ src/components/login.vue
@@ -0,0 +1,18 @@
+<template>
+<!-- 结构 -->
+    <div>
+        登陆组件
+    </div>
+</template>
+
+<script>
+// 行为
+export default {
+    
+}
+</script>
+<style lang="less" scoped>
+/* // 样式；lang:语法；scoped指令，控制样式生成，表示只在当前组件内生效；防止组件之间的样式冲突 */
+
+
+</style>
